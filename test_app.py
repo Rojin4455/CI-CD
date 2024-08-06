9ef4da7aea79a939de326f106848898ce4f405ef
Rojin4455 <rojinsaji35@gmail.com>
2024-08-06 18:39:54 +0530

    modified: add 'new'

--- test_app.py
+++ test_app.py
@@ -2,5 +2,5 @@ def test_home():
     from app import app
     client = app.test_client()
     response = client.get("/")
-    assert response.data == b"Hello, CI/CD!"
+    assert response.data == b"Hello, CI/CD! new"
 
